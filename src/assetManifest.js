32fd0de95d1ff540952ebc414cf9c90e67f5c4c6
Andrew Stelmach <andrew@volter.ai>
2025-03-26 01:40:45 -0400

    IDE file change

--- src/assetManifest.js
+++ src/assetManifest.js
@@ -12,6 +12,142 @@ export const ASSETS = {
       path: 'assets/backgrounds/mainMenuBackground.png',
       width: 390,  // use appropriate sizes for this game
       height: 844   // use appropriate sizes for this game
+    },
+    day: {
+      path: 'assets/backgrounds/day.png',
+      width: 1024,
+      height: 1024
+    },
+    night: {
+      path: 'assets/backgrounds/night.png',
+      width: 1024,
+      height: 1024
+    }
+  },
+  characters: {
+    sheriff: {
+      path: 'assets/characters/sheriff.png',
+      width: 128,
+      height: 128
+    },
+    outlaw: {
+      path: 'assets/characters/outlaw.png',
+      width: 128,
+      height: 128
+    },
+    bountyHunter: {
+      path: 'assets/characters/bountyHunter.png',
+      width: 128,
+      height: 128
+    },
+    dead: {
+      path: 'assets/characters/dead.png',
+      width: 128,
+      height: 128
+    }
+  },
+  buildings: {
+    saloon: {
+      path: 'assets/buildings/saloon.png',
+      width: 256,
+      height: 256
+    },
+    cactus: {
+      path: 'assets/buildings/cactus.png',
+      width: 128,
+      height: 256
+    }
+  },
+  weapons: {
+    revolver: {
+      path: 'assets/weapons/revolver.png',
+      width: 128,
+      height: 128
+    },
+    shotgun: {
+      path: 'assets/weapons/shotgun.png',
+      width: 128,
+      height: 128
+    },
+    rifle: {
+      path: 'assets/weapons/rifle.png',
+      width: 128,
+      height: 128
+    },
+    dynamite: {
+      path: 'assets/weapons/dynamite.png',
+      width: 128,
+      height: 128
+    }
+  },
+  bullets: {
+    revolver: {
+      path: 'assets/bullets/revolver.png',
+      width: 32,
+      height: 32
+    },
+    shotgun: {
+      path: 'assets/bullets/shotgun.png',
+      width: 32,
+      height: 32
+    },
+    rifle: {
+      path: 'assets/bullets/rifle.png',
+      width: 48,
+      height: 32
+    },
+    dynamite: {
+      path: 'assets/bullets/dynamite.png',
+      width: 64,
+      height: 64
+    }
+  },
+  powerUps: {
+    health: {
+      path: 'assets/powerups/health.png',
+      width: 64,
+      height: 64
+    },
+    speed: {
+      path: 'assets/powerups/speed.png',
+      width: 64,
+      height: 64
+    },
+    quickReload: {
+      path: 'assets/powerups/quickReload.png',
+      width: 64,
+      height: 64
+    }
+  },
+  items: {
+    gold: {
+      path: 'assets/items/gold.png',
+      width: 64,
+      height: 64
+    }
+  },
+  actions: {
+    shooting: {
+      path: 'assets/actions/shooting.png',
+      width: 128,
+      height: 128
+    },
+    reloading: {
+      path: 'assets/actions/reloading.png',
+      width: 128,
+      height: 128
+    }
+  },
+  ui: {
+    shoot: {
+      path: 'assets/ui/shoot.png',
+      width: 64,
+      height: 64
+    },
+    reload: {
+      path: 'assets/ui/reload.png',
+      width: 64,
+      height: 64
     }
   }
 };
